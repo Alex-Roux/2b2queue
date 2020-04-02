65588391ff77c148098b13a7d348a6cdb8b27be5
Alex <50831262+Alex-Roux@users.noreply.github.com>
2020-04-02 08:03:46 +0200

    Moved log function to main.js and added a thread indicating feature

--- src/main/main.js
+++ src/main/main.js
@@ -1,17 +1,27 @@
-const discord = require('./discordbot.js');
-const minecraft = require('./minecraft.js');
+const fs = require('fs');
 
-// const readline = require('readline').createInterface({
-  // input: process.stdin,
-  // output: process.stdout
-// })
 
-// while(1 == 1){
-
-// readline.question(``, (input) => {
-	// if(input == 'stop'){
-		// process.exit()
-	// }
-	// readline.close()
-// })
-// }
+exports.log = function(string, threadID) {
+	var date = ('[' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + ' GMT] ');
+	//console.log(threadID);
+	switch (threadID){
+		case 0: // General
+			var thread = '[General Thread] ';
+		break;
+		case 1: // Discord
+			var thread = '[Discord Thread] ';
+		break;
+		case 2: // Server
+			var thread = '[Server Thread] ';
+		break;
+		case 3: // Client
+			var thread = '[Client Thread] ';
+		break;
+		default:
+			var thread = '[Undefined thread] ';
+	};
+	var logLine = date + thread + string;
+	console.log(logLine);
+	
+	fs.appendFile('latest.log', logLine + "\r\n", function (err) {if (err) throw err;});
+}
