f1d4d2b43a0ff3bb6ea69b8a5cb74f37a375f253
Alex <50831262+Alex-Roux@users.noreply.github.com>
2020-04-02 08:06:07 +0200

    Clearing logs on startup, may be removed later

--- src/main/start.js
+++ src/main/start.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+
+fs.writeFile('latest.log', 'latest.log\r\n', (err) => {
+	if (err) throw err;
+});
+
 const discord = require('./discordbot.js');
 const minecraft = require('./minecraft.js');
 
